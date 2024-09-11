95aa4a127f1c01beec560fbd46880fc94db8d35f
Giribabu <kgiribabuk12@gmail.com>
2024-09-11 20:31:25 +0530

    Initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Java Developer", "UI/UX Designer" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Giri babu `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Java Developer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>
+                  Hello, I'm Giri Babu, a passionate web developer dedicated to transforming ideas into interactive and dynamic digital experiences. I specialize in HTML, CSS, JavaScript, and Java, bringing creativity and functionality to every project I undertake.
+                  <br />
+                  My journey in web development has led me to create innovative solutions like an online student feedback system, each project pushing the boundaries of creativity and user experience. I have successfully completed Web Development and Java training programs on Udemy, further honing my skills and expanding my technical knowledge.
+                  <br />
+                  Take a stroll through my portfolio to explore the diverse range of projects I've had the privilege to work on, and see firsthand how I turn concepts into reality.
+                  </p>
+                  
+                  <button onClick={() => window.open("/Resume/Giri babu_MCA_2023_Resume.pdf", "_blank", "noopener,noreferrer")}>Resume / CV <ArrowRightCircle size={25} /></button>
+                  
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,106 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import student from "../assets/img/student.png";
+import hospital from"../assets/img/hospital.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Online Student Feedback System",
+      description: "This project was designed to streamline the process of collecting and managing student feedback for educational institutions.",
+      imgUrl: student,
+    },
+    {
+      title: "To-Do-App",
+      description: "The Todo Application is a ReactJS-based task management tool that allows users to add, check the status of, and delete tasks. Users can create new tasks, mark them as completed or pending, and remove tasks once they are no longer needed. This simple and efficient app helps users stay organized and manage their daily tasks effectively.",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Hospital Management System",
+      description: "The Hospital Management System is a comprehensive web-based application designed to streamline the operations of a healthcare facility. The system consists of three main modules: Patient, Doctor, and Admin, each with distinct functionalities.",
+      imgUrl: hospital,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>
+                These are the projects I have completed, listed below.
+                </p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <p>
+
+                      </p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>
+
+                      </p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
